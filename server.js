a634809d0f635534683187d18174d48dcf665114
Jinhu Wang <471055787@qq.com>
2023-06-15 00:09:11 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
-  res.redirect(301, 'https://chataizoo.com');
+  res.redirect(301, 'https://chat.chataizoo.com');
 });
 
 app.listen(PORT, () => {
